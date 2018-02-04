7b3b7ea63b06bccaed9fa7c5234f25c24fcc5dcb
Sigi5mund <ajwright124@gmail.com>
2018-02-04 14:27:29 +0000

    API requests working, current session array working

--- public/index.js
+++ public/index.js
@@ -1,19 +1,46 @@
-// {"name":"diana","gender":"female","samples":36,"accuracy":100,"duration":"41ms"}
-// const MyKey = require('../apiKey.js');
 
+
+// {"name":"diana","gender":"female","samples":36,"accuracy":100,"duration":"41ms"}
+
+
+// Dealing with name and preventing API being requested on name that already has been queried.
 
 var arrayOfNames =[];
+var returnInfo =  {"name":"diana","gender":"female","samples":36,"accuracy":100,"duration":"41ms"};
 
-var queryName = function(name){
-  urlConstructor(name);
 
-  // makeRequest(url, requestComplete);
-}
+
+var searchInputText = function(name){
+    var upperName= capitalizeFirstLetter(name);
+    printToLatest(upperName);
+    if (isNameNew(upperName) == true)
+      return;
+    else {
+      arrayOfNames.push(upperName);
+      console.log(arrayOfNames);
+      updateArrayOfNamesList();
+      queryName(name);
+    }
+};
+
+var printToLatest = function(name){
+  var gender = returnInfo['gender'];
+  var samples = returnInfo['samples'];
+  var accuracy = returnInfo['accuracy'];
+  console.log(accuracy);
+  console.log(gender);
+  console.log(sample);
+  var output = document.querySelector('#output')
+  output.innerText ="Result: " + name + " : "+ gender + " with " + samples + " samples at " + accuracy + "%  accuracy"
+};
 
 var isNameNew = function(newName){
   var nameMatch = false;
   for (element of arrayOfNames){
     if (element == newName){
+      // var gender = element['gender'];
+      // var samples = element['samples'];
+      // var accuracy = element['accuracy'];
       return true;
     } else {
       var nameMatch = false;
@@ -22,37 +49,25 @@ var isNameNew = function(newName){
   return nameMatch;
 }
 
-var searchInputText = function(name){
-  var capitalizeFirstLetter =function(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1)};
-    var upperName= capitalizeFirstLetter(name);
-    console.log(isNameNew(upperName));
-    if (isNameNew(upperName) == true)
-      return;
-    else {
-      arrayOfNames.push(upperName);
-      console.log(arrayOfNames);
-      updateArrayOfNamesList(upperName);
-      printToLatest(upperName);
-      queryName(name);
-    }
-
-};
-
-var printToLatest = function(name){
-var gender ="";
-var output = document.querySelector('#output')
-output.innerText ="Latest Name Searched: " + name + " Gender: "+ gender;
-};
-
-var updateArrayOfNamesList= function(name){
+var updateArrayOfNamesList= function(){
   var arrayOutput= document.querySelector('#array-output');
   arrayOutput.innerHTML ='';
-  for (name of arrayOfNames){
+  let array1 = arrayOfNames.reverse();
+  for (element of array1){
     var newLi = document.createElement("li");
-    newLi.innerText= name;
+    newLi.innerText= element;
     arrayOutput.appendChild(newLi);
   }
+  array1.reverse();
+}
+
+
+
+// Dealing with API request and receiving reply
+
+var queryName = function(name){
+  urlConstructor(name);
+  makeRequest(url, requestComplete);
 }
 
 var makeRequest = function(url, callback){
@@ -66,9 +81,23 @@ var requestComplete = function() {
   if (this.status !== 200)
   return;
   var jsonString = this.responseText;
-  returnInfo = JSON.parse(jsonString);
+
+  // returnInfo = JSON.parse(jsonString);
   console.log(returnInfo);
 };
+
+var urlConstructor= function(name){
+  url = 'https://gender-api.com/get?key=' + MyKey + '&name=' + name;
+  console.log(url);
+  console.log(MyKey);
+  return url;
+};
+
+
+
+
+
+
 
 var app = function(){
   var searchBox = document.querySelector('#search-box')
@@ -81,17 +110,19 @@ var app = function(){
   })
 };
 
-var urlConstructor= function(name){
-  url = 'https://gender-api.com/get?key=' + MyKey + '&name=' + name;
-  console.log(url);
-  console.log(MyKey);
-  // return url;
-};
 
 
-// Gson gson = new Gson();
-// JsonObject json = gson.fromJson(reader, JsonObject.class);
-// String gender = json.get("gender").getAsString();
+var capitalizeFirstLetter =function(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)};
+
+
+
+
+// var jsonString = JSON.stringify(pet);
+// localStorage.setItem('pet', jsonString);
+
+// var jsonString = localStorage.getItem('pet');
+// var savedPet = JSON.parse(jsonString);
 
 
 window.addEventListener('load', app);
