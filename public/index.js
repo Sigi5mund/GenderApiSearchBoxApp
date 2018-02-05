9d46ae8a4ff9d3ac8baa0bdf7fc2a4dc6424b7de
Sigi5mund <ajwright124@gmail.com>
2018-02-05 08:31:52 +0000

    Functionally complete

--- public/index.js
+++ public/index.js
@@ -1,69 +1,33 @@
 
 
-// {"name":"diana","gender":"female","samples":36,"accuracy":100,"duration":"41ms"}
-
-
-// Dealing with name and preventing API being requested on name that already has been queried.
-
-
-
-
-var arrayOfHashObjects = [];
-
+// Global Variables
+var arrayOfCachedInfo = [];
 var arrayOfNamesSearchedThisSession =[];
-// var arrayOfHashObjects = [];
-// var diana = {"name":"diana","gender":"female","samples":36,"accuracy":100,"duration":"41ms"};
-// var andrew = {"name": "andrew", gender: "male", samples: 82448, accuracy: 99, duration: "40ms"};
-// arrayOfHashObjects.push(diana);
-// arrayOfHashObjects.push(andrew);
+
+// New Query to API
+
+var incomingInformationDissemination = function(incomingParsedData){
+  arrayOfCachedInfo.push(incomingParsedData);
+  console.log(incomingParsedData);
+  console.log(arrayOfCachedInfo);
+  addNameToLastestSearchesInfo(incomingParsedData);
+  printToLatestInfoBox(incomingParsedData);
+}
 
 
+// Dealing with previously entered names and displaying the information
 
 var searchExistingNames = function(typedName){
-  if (doesNameExist(typedName) == true){
-
+  if (doesNameExistInCachedNames(typedName) == true){
     return;
   } else {
     queryName(typedName);
-
   }
 }
 
-
-var addNameToLastestSearchesInfo= function(newElement){
-  var alreadyExists = false;
-  for (element of arrayOfNamesSearchedThisSession){
-    if (element['name'] == newElement['name']){
-      return alreadyExists = true;
-    } else {
-      alreadyExists = false;
-    }
-  }
-  if (alreadyExists == true) {
-    outputToArrayOfLatestSearches();
-  } else {
-    arrayOfNamesSearchedThisSession.push(newElement);
-    outputToArrayOfLatestSearches();
-  }
-}
-
-
-var outputToArrayOfLatestSearches = function(){
-  var arrayOutput= document.querySelector('#array-output');
-  arrayOutput.innerHTML ='';
-  let array1 = arrayOfNamesSearchedThisSession.reverse();
-  for (element of array1){
-    var newLi = document.createElement("li");
-    newLi.innerText = "name: " + element['name'] + " : "+ element['gender']+ " : " + element['samples'] + " : " + element['accuracy'] + "%  accuracy"
-    arrayOutput.appendChild(newLi);
-  }
-  array1.reverse();
-}
-
-
-var doesNameExist = function(typedName){
+var doesNameExistInCachedNames = function(typedName){
   var nameDoesntExist = false;
-  for (element of arrayOfHashObjects){
+  for (element of arrayOfCachedInfo){
     if (element['name'] == typedName){
       printToLatestInfoBox(element);
       addNameToLastestSearchesInfo(element);
@@ -75,22 +39,61 @@ var doesNameExist = function(typedName){
   return nameDoesntExist;
 }
 
+var isLastestSearchAlreadyInLatestSearches = function(latestSearch){
+    var alreadyExists = false;
+  for (element of arrayOfNamesSearchedThisSession){
+    if (element['name'] == latestSearch['name']){
+      return alreadyExists = true;
+    } else {
+      alreadyExists = false;
+    }
+  }
+}
+
+
+// Output of data to the webpage
+
+var addNameToLastestSearchesInfo= function(newElement){
+  if (isLastestSearchAlreadyInLatestSearches(newElement) == true) {
+    outputToArrayOfLatestSearches();
+
+  } else {
+    arrayOfNamesSearchedThisSession.push(newElement);
+    outputToArrayOfLatestSearches();
+
+  }
+}
+
+var outputToArrayOfLatestSearches = function(){
+  var latestSearchList= document.querySelector('#array-output');
+  latestSearchList.innerHTML ='';
+  let arrayReversed = arrayOfNamesSearchedThisSession.reverse();
+  for (element of arrayReversed){
+    var newLi = document.createElement("li");
+    newLi.innerText = "name: " + element['name'] + " : "+ element['gender']+ " : " + element['samples'] + " : " + element['accuracy'] + "%  accuracy"
+    latestSearchList.appendChild(newLi);
+  }
+  arrayReversed.reverse();
+}
+
 var printToLatestInfoBox = function(nameInfoToPrint){
   var objectToPrint = nameInfoToPrint;
-  var name = objectToPrint['name'];
   var output = document.querySelector('#output')
   output.innerText ="Result: " + objectToPrint['name'] + " : "+ objectToPrint['gender']+ " with " + objectToPrint['samples'] + " samples at " + objectToPrint['accuracy'] + "%  accuracy"
 };
-
 
 
 // Dealing with API request and receiving reply
 
 var queryName = function(name){
   urlConstructor(name);
-  console.log('API has been pinged!');
   makeRequest(url, requestComplete);
 }
+
+var urlConstructor= function(name){
+  url = 'https://gender-api.com/get?key=' + MyKey + '&name=' + name;
+  return url;
+};
 
 var makeRequest = function(url, callback){
   var request = new XMLHttpRequest();
@@ -103,44 +106,43 @@ var requestComplete = function() {
   if (this.status !== 200)
   return;
   var jsonString = this.responseText;
-  returnInfo = JSON.parse(jsonString);
-  arrayOfHashObjects.push(returnInfo);
-  console.log(returnInfo);
-  console.log(arrayOfHashObjects);
-  addNameToLastestSearchesInfo(returnInfo);
-  printToLatestInfoBox(returnInfo);
+  parsedInfo = JSON.parse(jsonString);
+  incomingInformationDissemination(parsedInfo);
+  localStorageUpdate();
+};
+
+
+// Local Storage Retrieval and Updating
+
+var localStorageUpdate = function(){
   let jsonString2 = JSON.stringify(arrayOfHashObjects);
   localStorage.setItem('namesGenders', jsonString2);
-};
+}
 
-var urlConstructor= function(name){
-  url = 'https://gender-api.com/get?key=' + MyKey + '&name=' + name;
-  console.log(url);
-  console.log(MyKey);
-  return url;
-};
-
-var app = function(){
-  var searchBox = document.querySelector('#search-box')
-   window.addEventListener('keyup', function(e){
-    if (e.keyCode == 13){
-      var nameTyped = searchBox.value;
-      searchExistingNames(nameTyped);
-      searchBox.value ="";
-    }
-
-  })
+var retrieveInfoFromLocalStorage = function(){
   var jsonString1 = localStorage.getItem('namesGenders');
   arrayOfHashObjects = JSON.parse(jsonString1);
   console.log(arrayOfHashObjects);
+}
+
+
+// EventListener and App
+
+var respondToEnterKey = function(event){
+  var searchBox = document.querySelector('#search-box')
+  if (event.keyCode == 13){
+    var nameTyped = searchBox.value;
+    var downcasedNameTyped = nameTyped.toLowerCase();
+    searchExistingNames(downcasedNameTyped);
+    searchBox.value ="";
+  }
+}
+
+var app = function(){
+  retrieveInfoFromLocalStorage();
+  window.addEventListener('keyup', function(event){
+    respondToEnterKey(event)
+  })
 };
 
-
-
-
-
 window.addEventListener('load', app);
-
-
-// var capitalizeFirstLetter =function(string) {
-// return string.charAt(0).toUpperCase() + string.slice(1)};
